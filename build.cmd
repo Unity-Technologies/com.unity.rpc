baf8a0132737a5c65d5dc3ebd77a70bcba557508
Andreia Gaita <shana@spoiledcat.net>
2019-11-10 21:40:58 +0200

    ci: fix package versioning when building in yamato from master

--- build.cmd
+++ build.cmd
@@ -1,11 +1,20 @@
-@echo on
+@echo off
 setlocal
 
-set Configuration=Debug
-if not %1.==. (
-	set Configuration=%1
+SET CONFIGURATION=Debug
+SET PUBLIC=""
+
+:loop
+IF NOT "%1"=="" (
+    IF "%1"=="-p" (
+        SET PUBLIC=/p:PublicRelease=true
+    )
+    IF "%1"=="-r" (
+        SET CONFIGURATION=Release
+    )
+    SHIFT
+    GOTO :loop
 )
 
 dotnet restore
-dotnet build --no-restore -c %Configuration%
-
+dotnet build --no-restore -c %CONFIGURATION% %PUBLIC%
--- pack.cmd
+++ pack.cmd
@@ -1,10 +1,28 @@
-@echo on
+@echo off
 setlocal
 
-set Configuration=Debug
-if not %1.==. (
-	set Configuration=%1
+SET CONFIGURATION=Debug
+SET PUBLIC=""
+SET BUILD=0
+
+:loop
+IF NOT "%1"=="" (
+    IF "%1"=="-p" (
+        SET PUBLIC=/p:PublicRelease=true
+    )
+    IF "%1"=="-b" (
+        SET BUILD=1
+    )
+    IF "%1"=="-r" (
+        SET CONFIGURATION=Release
+    )
+    SHIFT
+    GOTO :loop
 )
 
-dotnet pack --no-build --no-restore -c %Configuration%
+if "x%BUILD%"=="x1" (
+	dotnet restore
+	dotnet build --no-restore -c %CONFIGURATION% %PUBLIC%
+)
 
+dotnet pack --no-restore --no-build -c %CONFIGURATION% %PUBLIC%
